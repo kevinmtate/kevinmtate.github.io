454022888293f1de7bea9eb9ffc25ef51ca544e2
Kevin Tate <2kmtate@gmail.com>
2020-02-18 21:45:11 -0500

    go to sleep

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,43 @@
   document.addEventListener("DOMContentLoaded", function() {
     // addEventHandlers();
 
-    const vpw = window.innerWidth;
-    const vph = window.innerHeight;
-    const diff = vpw - vph;
+    init();
+  });
 
+  const init = () => {
+    initContentWidth();
+
+    const contentEl   = document.querySelector('.content');
+    const titleEl     = document.querySelector('header');
+    const navEl       = document.querySelector('nav');
+    const background  = document.querySelector('.background');
+
+    contentEl.addEventListener('scroll', () => {
+      if (contentEl.scrollTop) {
+        titleEl.classList.add('title--scrolled');
+        navEl.classList.add('nav--scrolled');
+        background.classList.add('background--scrolled');
+      } else {
+        titleEl.classList.remove('title--scrolled');
+        navEl.classList.remove('nav--scrolled');
+        background.classList.remove('background--scrolled');
+      }
+    });
+  }
+
+  const initContentWidth = () => {
+    const diff     = window.innerWidth - window.innerHeight;
     const sections = document.querySelectorAll('.content section');
+
     [...sections].forEach(el => {
       el.style.transform = 'rotate(90deg) translateY(' + diff / 2 + 'px)';
       el.style.marginTop = diff + 'px';
     })
     sections[0].style.marginTop = diff / 2 + 'px';
-  });
+  }
+
+
+
 
   const addEventHandlers = () => {
 
