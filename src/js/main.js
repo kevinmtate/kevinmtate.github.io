c16da00f5d770d4b0fde74c1c19ae17e318f57d6
Kevin Tate <2kmtate@gmail.com>
2020-03-13 15:27:10 -0400

    front page

--- src/js/main.js
+++ src/js/main.js
@@ -5,43 +5,7 @@
   });
 
   const init = () => {
-    const nav = document.querySelector('nav');
-    const newStickyNav = new StickyNav(nav);
 
-    newStickyNav.init();
-  }
-
-  class StickyNav {
-    constructor(navEl) {
-      this.el = navEl;
-      this.position = navEl.getBoundingClientRect();
-      this.prevScrollY = 0;
-    }
-    
-    addScrollEventListener() {
-      window.addEventListener('scroll', () => this.setAsSticky());
-    }
-    
-    setAsSticky() {
-      const isBottom = window.scrollY >= this.position.bottom;
-      const isTop = window.scrollY <= this.position.top;
-      const scrollDown = window.scrollY > this.prevScrollY;
-      const scrollUp = !scrollDown;
-      
-      if (scrollDown && isBottom) {
-        this.el.classList.add('sticky');
-      }
-      
-      if (scrollUp && isTop) {
-        this.el.classList.remove('sticky');
-      }
-      
-      this.prevScrollY = window.scrollY;
-    }
-    
-    init() {
-      this.addScrollEventListener();
-    }
   }
   
 })();
